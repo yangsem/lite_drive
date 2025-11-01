e222f2fa33fdfc1f52a13db134b6dc72f0c83d76
yangsem <yangsemcs@gmail.com>
2025-11-01 19:09:19 +0800

    build(cmake): add CMakeList.txt

---
+++ sources/client/cmd/main.cpp
@@ -0,0 +1,12 @@
+// Lite Drive Client - Main Entry Point
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    std::cout << "Lite Drive Client" << std::endl;
+    // TODO: 实现客户端逻辑
+    (void)argc;
+    (void)argv;
+    return 0;
+}
+
---
+++ sources/server/main.cpp
@@ -0,0 +1,12 @@
+// Lite Drive Server - Main Entry Point
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    std::cout << "Lite Drive Server" << std::endl;
+    // TODO: 实现服务器逻辑
+    (void)argc;
+    (void)argv;
+    return 0;
+}
+
